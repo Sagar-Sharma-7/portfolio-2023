c07ca38d860cd01816bd983cc258239d33324492
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2023-08-04 16:51:30 +0530

    homepage ui design complete

--- public/script/coverpage.js
+++ public/script/coverpage.js
@@ -3,13 +3,14 @@ const coverPageToggle = () => {
     const coverPageWrap = document.querySelector(".coverpage_wrap");
     let i = 0;
     const coverpageInterval=  setInterval(() => {
-        if(i <= 100){
+        if(i <= 150){
             coverPageWrap.style.top = `-${i}%`;
             coverPageWrap.style.borderRadius = `${i}%`;
             console.log(i)
             i++;
         }else{
             console.log("else statement");
+            coverPageWrap.style.zIndex = "-1"
             clearInterval(coverpageInterval)
         }
     }, 10);
