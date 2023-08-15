9d2ad60a124a3337fc273aaa0655e7179707ffeb
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2023-08-15 16:32:49 +0530

    text typing and deleting effect

--- public/script/coverpage.js
+++ public/script/coverpage.js
@@ -1,20 +1,42 @@
-// cover page toggle up animation
-const coverPageToggle = () => {
-    const coverPageWrap = document.querySelector(".coverpage_wrap");
-    let i = 0;
-    const coverpageInterval=  setInterval(() => {
-        if(i <= 150){
-            coverPageWrap.style.top = `-${i}%`;
-            console.log(i)
-            i++;
-        }else{
-            console.log("else statement");
-            coverPageWrap.style.zIndex = "-1"
-            clearInterval(coverpageInterval)
-        }
-    }, 1);
-}
+//my name is Sagar Sharma and I am a freelance
+// const cursorElement = document.querySelector(".cursor");
+const textElement = document.querySelector(".typingeffect");
+const words = ["Hello World", "My name is Sagar Sharma and I am a freelance"];
+let i = 0;
+let timer;
 
-setTimeout(() => {
-    coverPageToggle();
-}, 1000);
+function typingEffect() {
+	let word = words[i].split("");
+	var loopTyping = function() {
+		if (word.length > 0) {
+			textElement.innerHTML += word.shift();
+		} else {
+			deletingEffect();
+			return false;
+		};
+		timer = setTimeout(loopTyping, 75);
+	};
+	loopTyping();
+};
+
+function deletingEffect() {
+	let word = words[i].split("");
+	var loopDeleting = function() {
+		if (word.length > 0) {
+			word.pop();
+			textElement.innerHTML = word.join("");
+		} else {
+			if (words.length > (i + 1)) {
+				i++;
+			} else {
+				i = 0;
+			};
+			typingEffect();
+			return false;
+		};
+		timer = setTimeout(loopDeleting, 50);
+	};
+	loopDeleting();
+};
+
+typingEffect();
