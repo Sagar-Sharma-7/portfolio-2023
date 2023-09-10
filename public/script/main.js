c150e18a7215fc029dd4c43fa5511b7e4c484f3a
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2023-09-10 11:24:08 +0530

    added gsap animation for coverpage

--- public/script/main.js
+++ public/script/main.js
@@ -2,6 +2,31 @@ const hamburgerBtn = document.querySelector(".hamburger");
 const closeBtn = document.querySelector("#close_btn");
 const nav = document.querySelector("nav");
 
+// gsap
+const tl = gsap.timeline({
+    defaults: {ease: 'power1.out'}
+});
+
+tl.to('.text', {
+    y:'0%',
+    duration: 0.5,
+    stagger: 0.25
+});
+tl.to(".slider", {
+    y: '-100%',
+    duration:1,
+    delay: 0.25
+})
+tl.to(".coverpage_wrap", {
+    y:'-100%',
+    duration: 1.5
+}, "-=1")
+
+
+
+
+
+// hamburger navbar
 $(document).ready(function(){
     $(".hamburger").click(function(){
         $("nav").slideDown(500);
@@ -11,3 +36,4 @@ $(document).ready(function(){
         $("nav").slideUp(500);
     })
 });
+
